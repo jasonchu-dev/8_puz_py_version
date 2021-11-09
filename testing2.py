0ec9690a389d9862bc2602e9f97f884909bfe263
jasonchu-dev <chujason717@gmail.com>
2021-11-08 16:56:07 -0800

    comments needed for testing2.py

--- testing2.py
+++ testing2.py
@@ -1,6 +1,7 @@
-import collections
+import heapq
 import time
 import copy
+# variables for main to print
 path = []
 expanded = 0
 q_size = 0
@@ -12,6 +13,7 @@ class ucs:
         self.queue = []
         self.checked = []
         self.puzzle = puzzle
+        # initial puzzle into queue
         self.queue.append(self.puzzle)
         self.found = False
         global start_time
@@ -19,31 +21,52 @@ class ucs:
         global expanded
         global q_size
     
-        start_time = time.time() # i would think clock starts here the moment first node is in queue
+        # i would think clock starts here the moment first node is in queue    
+        start_time = time.time()
+        # nothing in queue? then return false for no solution
+        if len(self.queue) == 0: return False
+        # but if there is, then continue checking for a solution
         while range(len(self.queue)) != 0:
+        # grab and track record queue size
             if q_size < len(self.queue):
                 q_size = len(self.queue)
+            # check the smallest object
             curr = self.queue[0]
+            # is it a goal?
             if curr.goal():
+                # the moment goal is found, then timer for search stops
                 finish_time = time.time() - start_time
+                # start traversing back to add solution
                 path.append(curr)
                 while curr.parent != None:
                     curr = curr.parent
                     path.insert(0, curr)
+                # checked already, put in here
                 self.checked.append(curr)
+                # true if solution is found
                 self.found = True
+                # now get out of this class
                 break
+            # checked already, put in here
             self.checked.append(curr)
-            if curr.parent == None: # for very root of node that has no parent, set heuristic
+            # for very root of node that has no parent, set heuristic
+            if curr.parent == None: 
                 curr.f = curr.h # or 0 since root is at depth 0
+            # expand
             curr.move()
+            # check parent objects to calculate heuristics and to check whether it should be appended to queue to avoid repeats
             for i in range(len(curr.kids)):
                 kid = curr.kids[i]
-                expanded += 1 # not sure if this includes repeat nodes
+                # track number of nodes expanded
+                expanded += 1
+                # heuristics calculation
                 kid.g = kid.parent.g + 1
                 kid.f = kid.g + kid.h
+                # repeat object?
                 if not self.prev_encounter_check(self.queue, kid) and not self.prev_encounter_check(self.checked, kid):
+                    # then add
                     self.queue.append(kid)
+            # take object out of queue since we are done checking it
             del self.queue[0]
 
     def prev_encounter_check(self, list, kid):
@@ -66,10 +89,10 @@ class a_star:
 
         start_time = time.time() # i would think clock starts here the moment first node is in queue
         while range(len(self.queue)) != 0:
+            heapq.heapify(self.queue)
             if q_size < len(self.queue):
                 q_size = len(self.queue)
-            self.queue = collections.deque(sorted(list(self.queue), key=lambda node : node.f))
-            curr = self.queue[0]
+            curr = heapq.heappop(self.queue)
             if curr.goal():
                 finish_time = time.time() - start_time
                 path.append(curr)
@@ -90,8 +113,7 @@ class a_star:
                 kid.g = kid.parent.g + 1
                 kid.f = kid.g + kid.h
                 if not self.prev_encounter_check(self.queue, kid) and not self.prev_encounter_check(self.checked, kid):
-                    self.queue.append(kid)
-            del self.queue[0]
+                    heapq.heappush(self.queue, kid)
 
     def prev_encounter_check(self, list, kid):
         for node in range(len(list)):
@@ -111,6 +133,9 @@ class node:
         self.g = 0 # cost from initial
         self.f = 0 # heuristic total
         self.val = val # user input taken for which hueristic to calculate h
+
+    def __lt__(self, other):
+        return self.f < other.f
 
     def calc_h(self):
         for row in range(3):
@@ -195,7 +220,7 @@ x, y = 3, 3
 puzzle = [[0 for i in range(x)] for j in range(y)]
 spot = 0
 
-print("Fill in the array:\n")
+print("\nFill in the array:\n")
 
 for i in range(len(puzzle)):
     for j in range(len(puzzle)):
@@ -208,7 +233,7 @@ for i in range(len(puzzle)):
     print(puzzle[i])
 
 print("\n")
-val = int(input("Press 1 for Uniformed Cost Search\nPress 2 for A* with Manhattan Distance heuristic\nPress 3 for A* with Misplaced Tiles heuristic\n"))
+val = int(input("Press 1 for Uniform Cost Search\nPress 2 for A* with Manhattan Distance heuristic\nPress 3 for A* with Misplaced Tiles heuristic\n"))
 
 print("Please wait...")
 
@@ -222,7 +247,7 @@ if val == 2 or val == 3:
     else: str = "A* with Misplaced Tiles heuristic\n"
     search = a_star(p)
 else: 
-    str = "Uniformed Cost Search\n"
+    str = "Uniform Cost Search\n"
     search = ucs(p)
 
 if not search.found:
@@ -231,6 +256,8 @@ else:
     print("\nSolution:\n")
     for i in range(len(path)):
         print("Depth:", i)
+        print("g(n) =", path[i].g)
+        print("h(n) =", path[i].h)
         for j in range(len(path[i].puzzle)):
             print(path[i].puzzle[j])
         print('\n')
